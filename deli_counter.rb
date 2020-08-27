fab2534b3ce8876d465d0d1df403d6eef12200c8
Jc Dior <jessicaclairdior@gmail.com>
2020-08-27 17:40:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,7 @@ end
 
   def now_serving(katz_deli)
     if katz_deli.length > 0
+      katz_deli.shift
     puts "Currently serving #{katz_deli[0]}."
   else puts "There is nobody waiting to be served!"
   end
