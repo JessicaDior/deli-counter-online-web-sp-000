50b5eb284ed832125d4599b5d98142a4412f594e
Jc Dior <jessicaclairdior@gmail.com>
2020-08-27 17:37:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,8 +18,8 @@ def take_a_number(katz_deli, new_customer)
   puts "Welcome, #{new_customer}. You are number #{number + 1} in line."
 end
 
-  def now_serving(customer)
-    if customer.length >= 0
+  def now_serving(katz_deli)
+    if katz_deli.length > 0
     puts "The line is currently:#{katz_deli[0]}"
   else puts "There is nobody waiting to be served!"
   end
