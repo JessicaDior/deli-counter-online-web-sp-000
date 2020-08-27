75dd7b5f8ffc00cfaa41ad7396e249762a19c3f4
Jc Dior <jessicaclairdior@gmail.com>
2020-08-27 17:27:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ else
 end
 
 def take_a_number(katz_deli, new_customer)
-  katz_deli.length = number
+  number = katz_deli.length 
   katz_deli<<new_customer
   puts "Welcome #{new_customer}. You are number #{number + 1} in line."
 end
