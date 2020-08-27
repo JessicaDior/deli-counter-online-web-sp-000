888b11517d1a841e31063ab3a1ba0908866100ab
Jc Dior <jessicaclairdior@gmail.com>
2020-08-27 17:16:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  phrase = "The line is currently:"
+  if katz_deli.length > 0
+    katz_deli.each_with_index do |name, number|
+      phrase += " #{number + 1}. #{name}"
+    end
+  puts phrase
+else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, customer)
+  puts "Welcome #{customer}. You are number #{number} in line."
+end
+
+  def now_serving
+    if katz_deli.length >= 0
+    puts "The line is currently: #{line(customer)}"
+  else puts "There is nobody waiting to be served!"
+  end
+end
